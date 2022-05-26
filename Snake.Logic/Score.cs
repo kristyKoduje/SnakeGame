64eb3e07380743fa9f72ec13be1ef7790eecb5a7
Kristýna Moravcová <kmoravcova1997@gmail.com>
2022-05-26 16:27:04 +0200

    Added class score

---
+++ Snake.Logic/Score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Logic
+{
+    class Score : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int NumberOfEatenFoods { get; private set; }
+        public int NumberOfDeadSnakes { get; private set; }
+
+        public Score()
+        {
+            NumberOfEatenFoods = 0;
+            NumberOfDeadSnakes = 0;
+        }
+    }
+}
